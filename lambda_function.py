8104cce60f0b873a80a5e565a6cbcf3c66088593
Kelvinskell <kelvinskelll@gmail.com>
2022-08-05 05:54:21 +0100

    feat: Set logging functionality

--- lambda_function.py
+++ lambda_function.py
@@ -3,5 +3,12 @@
 import boto3
 import logging
 
+# Connect to AWS client endpoint
+client = boto3.client('ec2')
+
+# Set logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 def lambda_handler(event, context):
-    pass
+    
